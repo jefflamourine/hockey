1cc124962bf0d49ba5b016deb8b5387de7f521f4
Jeff Lamourine <jlamourine@placester.com>
2015-05-13 12:22:27 -0400

    Saving blank player

--- server.js
+++ server.js
@@ -491,7 +491,7 @@ var App = function() {
 		assists: 0
 	});
 
-	players = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18, player19, player20, player21, player22, player23, player24, player25, player26, player27, player28, player29, player30, player31, player32, player33, player34, player35, player36, player37, player38, player39, player40, player41, player42, player43, player44, player45, player46, player47, player48];
+	players = [blankPlayer, player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18, player19, player20, player21, player22, player23, player24, player25, player26, player27, player28, player29, player30, player31, player32, player33, player34, player35, player36, player37, player38, player39, player40, player41, player42, player43, player44, player45, player46, player47, player48];
 
 	var WPG = new Team({
 		name: "Winnipeg Jets",
